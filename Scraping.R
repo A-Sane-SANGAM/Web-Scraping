6f5854b7251c7f9959c90a024b0861fac7123425
Sangam Kumar <47377432+anonymously-sane@users.noreply.github.com>
2020-12-20 01:49:20 +0530

    Add files via upload

--- Scraping.R
+++ Scraping.R
@@ -1,58 +1,158 @@
-# install.packages("tidyverse")
-# install.packages("rvest")
-# install.packages("httr")
-# install.packages("RSelenium")
-# install.packages("lubridate")
-
 library(tidyverse)
 library(rvest)
 library(httr)
 library(RSelenium)
 library(lubridate)
 
-
+#Requesting a connection
 binman::list_versions("chromedriver")
-con <- rsDriver(browser="chrome", port=4444L, chromever="87.0.4280.88" )
+con <- rsDriver(browser="chrome", port=4444L, chromever="87.0.4280.88")
 conDriver <- con[["client"]]
 
-# KILLING A PROCESS ON A PORT IN CMD
-# netstat -ano | findstr:4444
-# Identify the PID which is in use.
-# taskkill /PID <PID in use> /F
+#Getting the list of products
+product_df <- read.csv("product_list.csv")
 
-
-#Navigating a URL
+#Navigating the website
 conDriver$navigate("https://www.walmart.com/")
-#Getting the navigated URL
-conDriver$getCurrentUrl()
-#Going back to the last URL
-conDriver$goBack()
-#Going to a forward URL if it exists
-conDriver$goForward()
-#Refreshing a URL
-conDriver$refresh()
+Sys.sleep(30)
 
-# SEARCHING ELEMENTS ON A WEB PAGE ****************************************************************
-#Searching element by name
-searchbox_ByName <- conDriver$findElement(using = "name",value = "query")
-#Searching element by class
-searchbox_ByClass <- conDriver$findElement(using = "class",value = "header-GlobalSearch-input")
-#Searching element by ID
-searchbox_ByID <- conDriver$findElement(using = "id",value = "global-search-input")
-#Searching element by CSS Selector
-searchbox_By_CSS_Selector1 <- conDriver$findElement(using = "css", "[name='query']")
-searchbox_By_CSS_Selector2 <- conDriver$findElement(using = "css", "[class='header-GlobalSearch-input']")
-searchbox_By_CSS_Selector3 <- conDriver$findElement(using = "css", "[id='global-search-input']")
-#Searching element by xpath
-searchbox_By_XPath1 <- conDriver$findElement(using = "xpath","//*[@id='global-search-input']")
-searchbox_By_XPath2 <- conDriver$findElement(using = "xpath","//*[@class='header-GlobalSearch-input']")
-searchbox_By_XPath1$getElementAttribute("class")
 
-# SENDING EVENTS TO ELEMENTS  *********************************************************************
-#Sending a string to the search box element and hit enter
-searchbox_ByName$sendKeysToElement(list("5-hour ENERGY Shot, Extra Strength, Grape, 1.93 oz","enter"))
-#This will give u the list of keys available for use.
-View(selKeys)
+getProductAttributes <- function(title){
+      
+    #Searching for the product on the website
+    searchbox_ByName <- conDriver$findElement(using = "name",value = "query")
+    searchbox_ByName$sendKeysToElement(list(title,key="enter"))
+    Sys.sleep(15)
+    
+    #Scraping product attributes from the results page
+    search_result_text <- conDriver$findElements(using = "xpath", value = "//*[@id='searchProductResult']/ul/li | //*[@id='searchProductResult']/div/div")
+    Sys.sleep(5)
+    temp1 <- list()
+    i<-1
+    while (i<=length(search_result_text)) {
+        content <- search_result_text[[i]]
+        temp1[i] <- content$getElementText()
+        i<-i+1
+    }
+    
+    #Scraping the links for the corresponding products
+    search_result_links <- conDriver$findElements(using = "class", value = "product-title-link")
+    Sys.sleep(5)
+    temp2 <- list()
+    i<-1
+    while (i<=length(search_result_text)) {
+      link <- search_result_links[[i]]
+      temp2[i] <- link$getElementAttribute("href")
+      i<-i+1
+    }
+    
+    #Appending the product data into product data frame
+    temp3 <- data.frame(unlist(temp1),unlist(temp2))
+    colnames(temp3) <- c('content','link')
+    #Removing the sponsored product
+    temp3 <- temp3[!grepl("Sponsored Product", temp3$content),]
+    #Keeping only those products where title is present
+    temp3 <- temp3[grepl("Product Title", temp3$content),]
+    temp3$title <- to_search
+    temp3 <- temp3[,c(3,1,2)]
+    
+    
+    #Going back to home page
+    conDriver$goBack()
+    Sys.sleep(30)
+    
+    return (temp3)
+
+}
+
+#Creating a data frame to store all product attributes
+product_df_final <- data.frame(matrix(ncol = 3))
+colnames(product_df_final) <- c("title","content","link")
+
+
+for(k in 1:nrow(product_df)){
+  to_search <- product_df[k,]
+  temp4 <- getProductAttributes(to_search)
+  product_df_final <- rbind(product_df_final,temp4)
+}
+
+#Removing top row from the final product table
+product_df_final <- product_df_final[-1,]
+
+
+
+
+# # OTHER CODES **********************************************************************************
+# 
+# # KILLING A PROCESS ON A PORT IN CMD
+# # netstat -ano | findstr :4444
+# # Identify the PID which is in use.
+# # taskkill /PID <PID in use> /F
+# 
+# #This will give u the list of keys available for use.
+# View(selKeys)
+# 
+# #Navigating a URL
+# conDriver$navigate("https://www.walmart.com/")
+# #Getting the navigated URL
+# conDriver$getCurrentUrl()
+# #Going back to the last URL
+# conDriver$goBack()
+# #Going to a forward URL if it exists
+# conDriver$goForward()
+# #Refreshing a URL
+# conDriver$refresh()
+# 
+# # SEARCHING ELEMENTS ON A WEB PAGE ****************************************************************
+# #Searching element by name
+# searchbox_ByName <- conDriver$findElement(using = "name",value = "query")
+# #Searching element by class
+# searchbox_ByClass <- conDriver$findElement(using = "class",value = "header-GlobalSearch-input")
+# #Searching element by ID
+# searchbox_ByID <- conDriver$findElement(using = "id",value = "global-search-input")
+# #Searching element by CSS Selector
+# searchbox_By_CSS_Selector1 <- conDriver$findElement(using = "css", "[name='query']")
+# searchbox_By_CSS_Selector2 <- conDriver$findElement(using = "css", "[class='header-GlobalSearch-input']")
+# searchbox_By_CSS_Selector3 <- conDriver$findElement(using = "css", "[id='global-search-input']")
+# #Searching element by xpath
+# searchbox_By_XPath1 <- conDriver$findElement(using = "xpath","//*[@id='global-search-input']")
+# searchbox_By_XPath2 <- conDriver$findElement(using = "xpath","//*[@class='header-GlobalSearch-input']")
+# searchbox_By_XPath1$getElementAttribute("class")
+# 
+# #Searching element by name
+# searchbox_ByName <- conDriver$findElement(using = "name",value = "query")
+# #Sending a string to the search box element and hit enter
+# searchbox_ByName$sendKeysToElement(list("Eternal Energy Shot Extra Strength Grape",key="enter"))
+# #Going to the product page of the first result
+# first_product <- conDriver$findElement(using = "class", value = "truncate-title")
+# first_product$sendKeysToElement(list(key="enter"))
+# 
+# #PRODUCT TITLE
+# product_title <- conDriver$findElement(using = "class", value = "prod-ProductTitle")
+# a$title <- product_title$getElementText()
+# #WALMART SKU
+# product_item_number <- conDriver$findElement(using = "class", value = "wm-item-number")
+# a$sku <- product_item_number$getElementText()
+# #PRODUCT PRICE
+# product_price <- conDriver$findElement(using = "class", value = "price-group")
+# a$price <-product_price$getElementText()
+# #PRODUCT STARS
+# product_star_rating <- conDriver$findElement(using = "xpath", value = "//*[@id='product-overview']/div/div[3]/div/div[1]/div[1]/div/button/span/span[1]")
+# a$stars <- product_star_rating$getElementText()
+# #PRODUCT REVIEWS
+# product_reviews <- conDriver$findElement(using = "class", value = "stars-reviews-count-node")
+# a$reviews <- product_reviews$getElementText()
+# #PRODUCT DESCRIPTION
+# product_description <- conDriver$findElement(using = "class", value = "about-product-description")
+# a$description <- product_description$getElementText()
+# 
+# conDriver$goBack()
+# conDriver$goBack()
+
+
+
+
+
 
 
 
